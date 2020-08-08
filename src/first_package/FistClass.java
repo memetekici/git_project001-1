340d4ae3c40429e99721db500f46dcc1146b40d6
Soner Afat <sonerafat@gmail.com>
2020-08-08 18:32:07 +0200

    May second commit from git repository

--- src/first_package/FistClass.java
+++ src/first_package/FistClass.java
@@ -4,6 +4,7 @@ public class FistClass {
 	
 	public static void main(String[] args) {
 		System.out.println("Our first commit from local repo");
+		System.out.println("Our second commit from local repository");
 	}
 
 }
