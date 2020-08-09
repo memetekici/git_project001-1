3e6f06cbf697dfa5fb25660a328caf7e84fb4901
Soner Afat <sonerafat@gmail.com>
2020-08-09 12:17:26 +0200

    last commit from local master branch

--- src/first_package/FistClass.java
+++ src/first_package/FistClass.java
@@ -6,6 +6,9 @@ public class FistClass {
 		System.out.println("Our first commit from local repo");
 		System.out.println("Our second commit from local repository");
 		System.out.println("Now I am on my afat branch");
+		System.out.println("I wan to have conflict");
+		
+		System.out.println("I want to have a second conflict");
 	}
 
 }
